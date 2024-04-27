b525be30094dd6c9ae5ab002fcbed8c1f8c15d40
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2024-04-27 18:49:28 -0300

    Update Converter.py

--- CONVERSÃO DE MOEDAS/Converter.py
+++ CONVERSÃO DE MOEDAS/Converter.py
@@ -1,44 +1,45 @@
+def subtrair(v1, v2, m1):
+    if v1 > v2:
+        troco = v1 - v2
+        return str(troco)+ "_" + m1
+    else:
+        return "Moedas Insuficientes"
 def converter(v1,v2,m1,m2 ):
-    if m1 == "PL":
-        # Se o pagamento for em peças de platina
-        if m2 == "PO":
-            return str((v1*10)-v2)+"_PO"
-        elif m2 == "PE":
-            return str((v1*20)-v2)+"_PE"
-        elif m2 == "PP":
-            return str((v1*100)-v2)+"_PP"
-        elif m2 == "PC":
-            return str((v1*100)-v2)+"_PC"
-    elif m1 == "PO":
-        # Se o pagamento for em peças de Ouro
-        if m2 == "PL":
-            convertido = v2*10
-            if v1==convertido:
-                return "0_"+m1
-            elif v1>(convertido):
-                return str(v1-convertido)+"_PO"
-            else:  return "Moedas Insuficientes"
-        if m2 == "PE":
-            return str((v1 * 2) - v2) + "_PE"
+    conversao = {
+        "PL": {"PO": 10, "PE": 20, "PP": 100, "PC": 100},
+        "PO": {"PL": 10, "PE": 2, "PP": 10, "PC": 100},
+        "PE": {"PL": 20, "PO": 2, "PP": 5, "PC": 50},
+    }
+    if m1 in conversao and m2 in conversao[m1]:
+        if m1 == "PL":
+            return v1 * conversao[m1][m2]
+        if m1 == "PO" and m2 == "PL":
+            return v2 * conversao[m2][m1]
+        if m1 == "PE" and (m2 == "PL" or m2 == "PO"):
+            return v2 * conversao[m2][m1]
+        if m1 == "PP" and (m2 == "PL" or m2 == "PO" or m2 == "PE"):
+            return v2 * conversao[m2][m1]
+        if m1 == "PC":
+            return v2 * conversao[m2][m1]
 
 def Troco(moeda1, moeda2):
     troco = 0
+    moeda1[1]= moeda1[1].upper()
     if moeda1 == moeda2:
         return troco+"_"+moeda1[1]
     v1 = int(moeda1[0])
     v2 = int(moeda2[0])
     if moeda1[1] == moeda2[1]:
-        if v1 > v2:
-            troco = v1 - v2
-            return str(troco)+"_"+moeda1[1]
-        else:
-            return "Moedas Insuficientes"
+      return subtrair(v1,v2)
     else:
-        return converter(v1,v2, moeda1[1].upper(), moeda2[1].upper())
+        v2 = converter(v1,v2, moeda1[1], moeda2[1].upper())
+        return subtrair(v1,v2, moeda1[1])
+def main():
+    entrada = "3_PE"
+    valor_a_pagar = "1_Po"
+    moeda1 = entrada.split("_")
+    moeda2 = valor_a_pagar.split("_")
 
-entrada = "10_Po"
-valor_a_pagar = "1_Pl"
-moeda1 = entrada.split("_")
-moeda2 = valor_a_pagar.split("_")
+    print(Troco(moeda1, moeda2))
 
-print(Troco(moeda1, moeda2))
+main()
