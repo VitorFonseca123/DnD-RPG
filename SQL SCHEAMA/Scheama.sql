9e526cb667a5695617846f0bfe2be406aa3f58ce
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2026-01-16 19:21:19 -0300

    Update Scheama.sql

--- SQL SCHEAMA/Scheama.sql
+++ SQL SCHEAMA/Scheama.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Escola_Magia (
     id_escola SERIAL PRIMARY KEY,
     nome_escola VARCHAR(50) NOT NULL
@@ -6,10 +7,11 @@ CREATE TABLE Classe (
     id_classe SERIAL PRIMARY KEY,
     nome_classe VARCHAR(50) NOT NULL,
 );
-CREATE TABLE habilidade_racial(
+CREATE TABLE habilidade(
 	id_habilidade serial PRIMARY KEY,
 	nome_skill VARCHAR(50) NOT NULL,
-	descricao text
+	descricao text,
+	lvl smallint 
 );
 
 
@@ -17,7 +19,7 @@ CREATE TABLE Raca (
     id_raca SERIAL PRIMARY KEY,
     nome_raca VARCHAR(50) NOT NULL,
     descricao_raca TEXT,
-    aprim_val_hab TEXT,
+    aprim_val_hab VARCHAR(2),
     tamanho VARCHAR(20),
     deslocamento INTEGER,
     proficiencias TEXT,
@@ -29,17 +31,22 @@ CREATE TABLE Sub_raca (
     id_raca INTEGER REFERENCES Raca(id_raca) ON DELETE CASCADE,
     nome_subraca VARCHAR(50),
     descricao_subraca TEXT,
-    aprim_val_hab TEXT,
+    aprim_val_hab VARCHAR(2),
     proficiencias TEXT,
 );
 
-CREATE TABLE habilidade_raca (
+CREATE TABLE Subclasse(
+	id_subclasse SERIAL PRIMARY KEY,
+	id_classe INTEGER REFERENCES Classe(id_classe) ON DELETE CASCADE,
+	nome_subclasse VARCHAR(50),
+	desc_subclasse TEXT
+);
+CREATE TABLE habilidade_origem (
     id_habilidade INTEGER REFERENCES habilidade_racial(id_habilidade),
     id_raca INTEGER REFERENCES Raca(id_raca),
-    PRIMARY KEY (id_habilidade, id_raca)
+	id_subraca INTEGER REFERENCES Sub_raca(id_subraca),
+	id_classe INTEGER REFERENCES Classe(id_classe),
+	id_subclasse INTEGER REFERENCES Subclasse(id_sublasse),
+    PRIMARY KEY (id_habilidade, id_raca, id_subraca, id_classe, id_subclasse)
 );
-CREATE TABLE habilidade_subraca (
-    id_habilidade INTEGER REFERENCES habilidade_racial(id_habilidade),
-    id_subraca INTEGER REFERENCES Raca(id_subraca),
-    PRIMARY KEY (id_habilidade, id_raca)
-);
+
